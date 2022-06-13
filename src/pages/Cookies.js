070516125bac49b3f2570b6c6f3df54bc2d1fa11
JRDNXr <jrdn_dsouza@hotmail.com>
2022-06-13 00:39:37 -0400

    1.21 - Minor grammatical change to footer text

--- src/pages/Cookies.js
+++ src/pages/Cookies.js
@@ -5,7 +5,7 @@ const Cookies = () => {
       <div className="Cookies">
         <h1>COOKIES</h1>
         <p className="spacingP">This page is dedicated to my loving Grandmother ❤️ whose favourite food,</p>
-        <p className="spacingP">morning, day, and night, was (you guessed it) <span id="cookies-text">COOKIES!</span></p>
+        <p className="spacingP">morning, day, and night, is (you guessed it) <span id="cookies-text">COOKIES!</span></p>
         <p></p>
       </div>
     );
