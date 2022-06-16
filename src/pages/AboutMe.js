1bc32d4db07889b503576bf4e7e4224034f18265
JRDNXr <jrdn_dsouza@hotmail.com>
2022-06-15 22:27:14 -0400

    Updated About Me page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -7,8 +7,8 @@ const AboutMe = () => {
             <h3>Hello stranger.</h3>
             <p>Or maybe you're not a stranger. Either way, welcome to my personal website! My name is <span id="name">Jordan D'Souza</span>, although I also
                use the alias "JRDN" as well. I created this website demonstrate and show off the incredible capabilities of coding and
-               programming. This About Me page is only the start - throughout the entire website, you will find several nods to my life
-               as a programmer and my life outside of a programmer. For those who know me personally, there are many little "easter eggs"
+               programming. This About Me page is only the start - throughout the entire website, you will find several nods to the many
+               hobbies and interests that I enjoy. For those who know me personally, there are many little "easter eggs"
                scattered throughout that I'm sure you will enjoy (SMILEY FACE).
             </p>
         </div>
